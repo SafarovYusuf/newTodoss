eb19e3a619f562802517f02f99ff0cdecbe84cb8
SafarovHusan200 <safarovhusan2000@gmail.com>
2026-07-23 15:51:49 +0500

    main

---
+++ src/components/Todo-contaner.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ToDoContainer() {
+  return <div>Todo-contaner</div>;
+}
+
+export default ToDoContainer;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import TodoContaner from "../components/Todo-contaner";
+
+function Home() {
+  return (
+    <>
+      <TodoContaner />
+    </>
+  );
+}
+
+export default Home;
